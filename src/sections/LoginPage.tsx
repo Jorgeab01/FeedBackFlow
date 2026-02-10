282cd0313a978b0bdf13308f0f493166111dfbf5
Jorge <jorgeab496@gmail.com>
2026-02-11 00:39:08 +0100

    Redirección al iniciar sesión

--- src/sections/LoginPage.tsx
+++ src/sections/LoginPage.tsx
@@ -35,7 +35,7 @@ export function LoginPage({ onLogin, themeProps }: LoginPageProps) {
       toast.success('¡Bienvenido!', {
         description: 'Has iniciado sesión correctamente.',
       });
-      navigate('/dashboard')
+      navigate('/dashboard', { replace: true });
     } else {
       toast.error('Error de inicio de sesión', {
         description: 'Email o contraseña incorrectos.',
